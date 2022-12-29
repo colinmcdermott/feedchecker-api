8afae3b39a2a39b3361ef36ebc0dda97caa2f869
Colin McDermott <colin.m.mcdermott@googlemail.com>
2022-12-29 22:40:46 +0000

    Create size.tsx

---
+++ pages/api/size.tsx
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import fetch from 'isomorphic-unfetch';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    // Get the URL of the RSS feed from the query parameters
+    const { feed } = req.query;
+
+    // Make a HEAD request to the RSS feed URL
+    const response = await fetch(feed, { method: 'HEAD' });
+
+    // Check the response status
+    if (response.status !== 200) {
+      // If the status is not 200 OK, return an error
+      res.status(response.status).json({ error: 'Failed to fetch feed' });
+      return;
+    }
+
+    // Get the Content-Length header from the response
+    const contentLength = response.headers.get('Content-Length');
+
+    // Return the Content-Length in JSON
+    res.json({ size: contentLength });
+  } catch (error) {
+    // If there was an error, return a server error
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
