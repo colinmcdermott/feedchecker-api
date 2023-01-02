022b4cfa52b170564d51aec21cdc7db6d19930f9
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-01-02 22:52:56 +0000

    Update size.tsx

--- pages/api/size.tsx
+++ pages/api/size.tsx
@@ -1,21 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import fetch from 'isomorphic-unfetch';
-import { parse as parseUrl } from 'url';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     // Get the URL of the RSS feed from the query parameters
     const { feed } = req.query;
-
-    // Parse the URL using the `url` module
-    const parsedUrl = parseUrl(feed);
-
-    // Check if the `parsedUrl` object has a `hostname` property
-    if (!parsedUrl.hostname) {
-      // If it does not, the `feed` query parameter is not a valid URL
-      res.status(400).json({ error: 'Invalid feed URL' });
-      return;
-    }
 
     // Make a GET request to the RSS feed URL
     const response = await fetch(`${feed}`);
