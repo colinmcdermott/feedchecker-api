e89c45f8c0615ad155a9143bd683554fe3f50a74
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-01-06 16:30:26 +0000

    1

--- pages/api/backend.tsx
+++ pages/api/backend.tsx
@@ -1,45 +1,9 @@
-import { GetServerSideProps } from 'next';
-import * as http from 'http';
-import * as url from 'url';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  let query = {};
-  if (context.req.url) {
-    query = url.parse(context.req.url, true).query;
-  }
-  let hubURL = '';
-  if (query.feed) {
-    hubURL = query.feed;
-  }
-
-  let size = null;
-  let error = null;
-
-  if (!hubURL) {
-    error = 'No feed URL specified';
-  } else {
-    const apiURL = `/api/size?feed=${hubURL}`;
-
-    try {
-      const apiRes = await http.get(apiURL);
-      let data = '';
-
-      apiRes.on('data', (chunk) => {
-        data += chunk;
-      });
-
-      apiRes.on('end', () => {
-        size = JSON.parse(data).size;
-      });
-    } catch (e) {
-      error = 'Could not get size of feed';
-    }
-  }
-
-  return {
-    props: {
-      size: size,
-      error: error,
-    },
-  };
-};
+export default async function feedSize(req: NextApiRequest, res: NextApiResponse) {
+  const { feed } = req.query;
+  const response = await fetch(`/api/size?feed=${feed}`);
+  const data = await response.json();
+  console.log(data.size);
+  res.status(200).end();
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,104 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import React, { useState, useEffect } from 'react';
+import Head from 'next/head';
 
-export default async function feedSize(req: NextApiRequest, res: NextApiResponse) {
-  const { feed } = req.query;
-  const response = await fetch(`/api/size?feed=${feed}`);
-  const data = await response.json();
-  console.log(data.size);
-  res.status(200).end();
+function FeedChecker() {
+  const [hubURL, setHubURL] = useState('');
+  const [feedSize, setFeedSize] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [change, setChange] = useState('');
+  const [debugLink, setDebugLink] = useState('');
+  const [lastPing, setLastPing] = useState('');
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setLoading(true);
+    const response = await fetch(`/api/size?feed=${hubURL}`);
+    const data = await response.json();
+    setFeedSize(data.size);
+    setLoading(false);
+    setDebugLink(`https://pubsubhubbub.appspot.com/topic-details?hub.url=${hubURL}`);
+  };
+
+  useEffect(() => {
+    if (feedSize !== null) { // only set up the interval if feedSize is not null
+      const interval = setInterval(async () => {
+        const response = await fetch(`/api/size?feed=${hubURL}`);
+        const data = await response.json();
+        if (data.size !== feedSize) {
+          setFeedSize(data.size);
+          setChange(`New feed size - Ping sent! - ${new Date().toUTCString()}`);
+          try {
+            // send ping to your new service
+            const pingResponse = await fetch(`/api/websub-ping?feed=${hubURL}`);
+            // handle pingResponse as needed
+            if (pingResponse.status !== 200) {
+              console.error(`Error pinging WebSub: ${pingResponse.status}`);
+              // handle error as needed
+            }
+          } catch (error) {
+            console.error(error);
+            // handle error as needed
+          }
+          // send ping to Google Sitemap API
+          const googlePingURL = `/api/google-ping?sitemap=${encodeURIComponent(hubURL)}`;
+          try {
+            const googlePingResponse = await fetch(googlePingURL);
+            // handle googlePingResponse as needed
+            if (googlePingResponse.status !== 200) {
+              console.error(`Error pinging Google: ${googlePingResponse.status}`);
+              // handle error as needed
+            }
+          } catch (error) {
+            console.error(error);
+            // handle error as needed
+          }
+          setLastPing(new Date().toUTCString());
+        } else {
+          setChange(`Feed size unchanged - ${new Date().toUTCString()}`);
+        }
+      }, 30000);
+      return () => clearInterval(interval);
+    }
+  }, [feedSize, hubURL]);
+
+  return (
+    <main>
+      <Head>
+        <title>Node Feed Checker</title>
+      </Head>
+      <form className='feedCheckForm' onSubmit={handleSubmit}>
+        <label htmlFor='hubURL'>RSS Feed URL:</label>
+        <input
+          type='url'
+          name='hub.url'
+          placeholder='https://example.com/feed/'
+          pattern='https://.*'
+          id='hubURL'
+          value={hubURL}
+          onChange={(event) => setHubURL(event.target.value)}
+          required
+        />
+        <input type='submit' value='Submit' id='submit' />
+      </form>
+
+      {loading && <div id='loading'><p>Connecting to API...</p></div>}
+      {feedSize && <div id='results'><p>API Success! Feed size: {feedSize} - Check interval: 30 seconds</p></div>}
+
+      <section className='statsWindow'>
+        <div id='change'>{change}</div>
+        {lastPing ? (
+          <div id='lastPing'>
+            Last ping sent: <time>{lastPing}</time>
+          </div>
+        ) : (
+          <div id='lastPing'></div>
+        )}
+      </section>
+
+      {debugLink && <div id='debug'><p><a href={debugLink} target='_blank'>Debug</a></p></div>}
+
+    </main>
+  );
 }
+
+export default FeedChecker;
