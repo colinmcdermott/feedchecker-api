50f7268086f9d7e17ca61a167f2f47c37b0d2589
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-01-06 16:05:15 +0000

    Update backend.tsx

--- pages/api/backend.tsx
+++ pages/api/backend.tsx
@@ -1,18 +1,29 @@
-import fetch from 'node-fetch';
-import { NextApiRequest, NextApiResponse } from 'next';
+const express = require('express');
 
-interface SizeResponse {
-  size: number;
+const app = express();
+
+async function fetchFeedSize(hubURL) {
+  try {
+    const response = await fetch(`/api/size?feed=${hubURL}`);
+    const data = await response.json();
+    return data.size;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  // Get the URL of the RSS feed or XML sitemap from the query string
-  const { hubURL } = req.query;
+app.get('/feed-size', async (req, res) => {
+  try {
+    const hubURL = req.query.feed;
+    const size = await fetchFeedSize(hubURL);
+    res.send({ size });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: 'An error occurred while fetching the feed size.' });
+  }
+});
 
-  // Make a GET request to your custom API to get the size of the feed
-  const response = await fetch(`/api/size?feed=${hubURL}`);
-  const data: SizeResponse = await response.json();
-
-  // Print the size value to the page
-  res.send(`The size of the feed is: ${data.size}`);
-};
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
