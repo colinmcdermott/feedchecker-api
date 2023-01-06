a985254a13543d8c66c352dbe6deb249dd2d7644
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-01-06 15:07:00 +0000

    Update backend.tsx

--- pages/api/backend.tsx
+++ pages/api/backend.tsx
@@ -10,53 +10,58 @@ const GOOGLE_PING_ENDPOINT = '/api/google-ping';
 let storedSizes: { [key: string]: number } = {};
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === 'GET' && req.query[HUB_URL_KEY]) {
-    const hubURL = req.query[HUB_URL_KEY] as string;
+  try {
+    if (req.method === 'GET' && req.query[HUB_URL_KEY]) {
+      const hubURL = req.query[HUB_URL_KEY] as string;
 
-    // Check if the hubURL is a valid URL
-    const parsedURL = url.parse(hubURL);
-    if (!parsedURL.protocol || !parsedURL.host) {
-      res.status(400).end('Invalid hubURL provided');
-      return;
-    }
+      // Check if the hubURL is a valid URL
+      const parsedURL = url.parse(hubURL);
+      if (!parsedURL.protocol || !parsedURL.host) {
+        res.status(400).end('Invalid hubURL provided');
+        return;
+      }
 
-    // Encode the hubURL value
-    const encodedHubURL = encodeURIComponent(hubURL);
+      // Encode the hubURL value
+      const encodedHubURL = encodeURIComponent(hubURL);
 
-    // Make request to custom API to get size of feed
-    const apiSizeEndpoint = `${API_SIZE_ENDPOINT}?feed=${encodedHubURL}`;
-    http.get(apiSizeEndpoint, apiRes => {
-      let data = '';
-      apiRes.on('data', (chunk: string) => {
-        data += chunk;
-      });
-      apiRes.on('end', () => {
-        const size = Number(data);
-        if (!size) {
-          res.status(400).end('Invalid size received from API');
-          return;
-        }
+      // Make request to custom API to get size of feed
+      const apiSizeEndpoint = `${API_SIZE_ENDPOINT}?feed=${encodedHubURL}`;
+      http.get(apiSizeEndpoint, apiRes => {
+        let data = '';
+        apiRes.on('data', (chunk: string) => {
+          data += chunk;
+        });
+        apiRes.on('end', () => {
+          const size = Number(data);
+          if (!size) {
+            res.status(400).end('Invalid size received from API');
+            return;
+          }
 
-        if (storedSizes[hubURL] === size) {
-          // Size has not changed, do nothing
-          res.status(200).end('Size has not changed');
-          return;
-        }
+          if (storedSizes[hubURL] === size) {
+            // Size has not changed, do nothing
+            res.status(200).end('Size has not changed');
+            return;
+          }
 
-        storedSizes[hubURL] = size;
+          storedSizes[hubURL] = size;
 
-        // Size has changed, ping WebSub API
-        const websubPingEndpoint = `${WEBSUB_PING_ENDPOINT}?feed=${encodedHubURL}`;
-        http.get(websubPingEndpoint, () => {
-          // Ping GooglePing API
-          const googlePingEndpoint = `${GOOGLE_PING_ENDPOINT}?sitemap=${encodedHubURL}`;
-          http.get(googlePingEndpoint, () => {
-            res.status(200).end('Size has changed and APIs have been pinged');
+          // Size has changed, ping WebSub API
+          const websubPingEndpoint = `${WEBSUB_PING_ENDPOINT}?feed=${encodedHubURL}`;
+          http.get(websubPingEndpoint, () => {
+            // Ping GooglePing API
+            const googlePingEndpoint = `${GOOGLE_PING_ENDPOINT}?sitemap=${encodedHubURL}`;
+            http.get(googlePingEndpoint, () => {
+              res.status(200).end('Size has changed and APIs have been pinged');
+            });
           });
         });
       });
-    });
-  } else {
-    res.status(400).end('No hubURL provided');
+    } else {
+      res.status(400).end('No hubURL provided');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).end('An error occurred');
   }
 };
