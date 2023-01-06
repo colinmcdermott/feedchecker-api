563b44b8ce7e69a97fe075dcae66b0e265427fb9
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-01-06 12:37:28 +0000

    Update google-ping.tsx

--- pages/api/google-ping.tsx
+++ pages/api/google-ping.tsx
@@ -3,6 +3,13 @@ import fetch from 'isomorphic-unfetch';
 
 export async function googlePingHandler(req: NextApiRequest, res: NextApiResponse) {
   const sitemapURL = req.query.feed;
+  try {
+    // validate the URL
+    new URL(sitemapURL);
+  } catch (error) {
+    console.error(`Invalid sitemap URL: ${sitemapURL}`);
+    // handle invalid URL as needed
+  }
   const googlePingURL = `https://www.google.com/ping?sitemap=${encodeURIComponent(sitemapURL)}`;
   try {
     const googlePingResponse = await fetch(googlePingURL);
