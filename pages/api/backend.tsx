c1a67a281ad192d7b4addb6ceb418c05e2603043
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-01-06 15:29:07 +0000

    Update backend.tsx

--- pages/api/backend.tsx
+++ pages/api/backend.tsx
@@ -1,5 +1,4 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import * as http from 'http';
 import * as url from 'url';
 
 const HUB_URL_KEY = 'hubURL';
@@ -9,7 +8,7 @@ const GOOGLE_PING_ENDPOINT = '/api/google-ping';
 
 let storedSizes: { [key: string]: number } = {};
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     if (req.method === 'GET' && req.query[HUB_URL_KEY]) {
       const hubURL = req.query[HUB_URL_KEY] as string;
@@ -26,37 +25,39 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
 
       // Make request to custom API to get size of feed
       const apiSizeEndpoint = `${API_SIZE_ENDPOINT}?feed=${encodedHubURL}`;
-      http.get(apiSizeEndpoint, apiRes => {
-        let data = '';
-        apiRes.on('data', (chunk: string) => {
-          data += chunk;
-        });
-        apiRes.on('end', () => {
-          const size = Number(data);
-          if (!size) {
-            res.status(400).end('Invalid size received from API');
-            return;
-          }
+      const apiRes = await fetch(apiSizeEndpoint);
+      const data = await apiRes.json();
+      const size = data.size;
+      if (!size) {
+        res.status(400).end('Invalid size received from API');
+        return;
+      }
 
-          if (storedSizes[hubURL] === size) {
-            // Size has not changed, do nothing
-            res.status(200).end('Size has not changed');
-            return;
-          }
+      if (storedSizes[hubURL] === size) {
+        // Size has not changed, do nothing
+        res.status(200).end('Size has not changed');
+        return;
+      }
 
-          storedSizes[hubURL] = size;
+      storedSizes[hubURL] = size;
 
-          // Size has changed, ping WebSub API
-          const websubPingEndpoint = `${WEBSUB_PING_ENDPOINT}?feed=${encodedHubURL}`;
-          http.get(websubPingEndpoint, () => {
-            // Ping GooglePing API
-            const googlePingEndpoint = `${GOOGLE_PING_ENDPOINT}?sitemap=${encodedHubURL}`;
-            http.get(googlePingEndpoint, () => {
-              res.status(200).end('Size has changed and APIs have been pinged');
-            });
-          });
-        });
-      });
+      // Size has changed, ping WebSub API
+      const websubPingEndpoint = `${WEBSUB_PING_ENDPOINT}?feed=${encodedHubURL}`;
+      const websubPingResponse = await fetch(websubPingEndpoint);
+      if (websubPingResponse.status !== 200) {
+        console.error(`Error pinging WebSub: ${websubPingResponse.status}`);
+        // handle error as needed
+      }
+
+      // Ping GooglePing API
+      const googlePingEndpoint = `${GOOGLE_PING_ENDPOINT}?sitemap=${encodedHubURL}`;
+      const googlePingResponse = await fetch(googlePingEndpoint);
+      if (googlePingResponse.status !== 200) {
+        console.error(`Error pinging Google: ${googlePingResponse.status}`);
+        // handle error as needed
+      }
+
+      res.status(200).end('Size has changed and APIs have been pinged');
     } else {
       res.status(400).end('No hubURL provided');
     }
