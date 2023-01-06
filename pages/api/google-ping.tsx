608d1b5b3e0f3967232ecbd9359ac482c306a02d
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-01-06 13:53:53 +0000

    Update google-ping.tsx

--- pages/api/google-ping.tsx
+++ pages/api/google-ping.tsx
@@ -19,7 +19,7 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
       throw new Error('Request failed');
     }
     res.status(200).json({ success: true });
-  } catch (error: Error) {
-    res.status(500).json({ success: false, error: error.message });
+    } catch (error) {
+    res.status(500).json({ success: false, error: (error as Error).message });
   }
 };
