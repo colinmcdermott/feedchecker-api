82cc4a6386dc87f439c4d5d13690922d9a3dc0b9
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-01-06 19:24:01 +0000

    Update backend.tsx

--- pages/api/backend.tsx
+++ pages/api/backend.tsx
@@ -1,21 +1,21 @@
-import { useState, useEffect } from 'react';
+import { NextApiRequest, NextApiResponse } from 'next';
+import fetch from 'isomorphic-unfetch';
 
-async function getFeedSize(hubURL: string) {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const response = await fetch(`/api/size?feed=${hubURL}`);
+    const { feed } = req.query;
+
+    // Make a request to the custom API to get the size of the feed
+    const response = await fetch(`/api/size?feed=${feed}`);
     const data = await response.json();
-    console.log(`Feed size: ${data.size}`);
+
+    // Print the size in the console
+    console.log(data.size);
+
+    // Respond with a success status and the size in the body
+    res.status(200).send({ size: data.size });
   } catch (error) {
-    console.error(error);
+    // If there was an error, respond with a 500 error
+    res.status(500).send({ error: error.message });
   }
 }
-
-function FeedChecker() {
-  const [hubURL, setHubURL] = useState('');
-
-  useEffect(() => {
-    getFeedSize(hubURL);
-  }, [hubURL]);
-}
-
-export default FeedChecker;
