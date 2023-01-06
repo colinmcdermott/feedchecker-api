feb3d9cb05cedf0417ad39ad49374f3f388190e3
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-01-06 11:29:42 +0000

    Update websub-ping.tsx

--- pages/api/websub-ping.tsx
+++ pages/api/websub-ping.tsx
@@ -4,7 +4,9 @@ import fetch from 'isomorphic-unfetch';
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   try {
     const { feed } = req.query;
-    if (!(feed instanceof URL)) {
+    const decodedFeed = decodeURIComponent(feed);
+    const feedUrl = new URL(decodedFeed);
+    if (!(feedUrl instanceof URL)) {
       throw new Error('Feed URL must be a valid URL');
     }
     const response = await fetch('https://pubsubhubbub.appspot.com/', {
@@ -12,7 +14,7 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
       },
-      body: `hub.mode=publish&hub.url=${encodeURIComponent(feed.toString())}`
+      body: `hub.mode=publish&hub.url=${encodeURIComponent(feedUrl.toString())}`
     });
     const data = await response.json();
     res.status(200).json({ success: true, data });
