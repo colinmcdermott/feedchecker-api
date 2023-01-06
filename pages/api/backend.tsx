26dc85a999c34e761592dc694645734fd51cea66
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-01-06 16:46:36 +0000

    Update backend.tsx

--- pages/api/backend.tsx
+++ pages/api/backend.tsx
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function feedSize(req: NextApiRequest, res: NextApiResponse) {
   const { feed } = req.query;
-  const response = await fetch(`/api/backend?feed=${feed}`);
+  const response = await fetch(`/api/size?feed=${feed}`);
   const data = await response.json();
   console.log(data.size);
   res.status(200).end();
