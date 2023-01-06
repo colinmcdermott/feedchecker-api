3e4c850a927efa726e6252c19d6199ecc7dbc378
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-01-06 15:48:25 +0000

    Update backend.tsx

--- pages/api/backend.tsx
+++ pages/api/backend.tsx
@@ -1,13 +1,17 @@
 import fetch from 'node-fetch';
 import { NextApiRequest, NextApiResponse } from 'next';
 
+interface SizeResponse {
+  size: number;
+}
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   // Get the URL of the RSS feed or XML sitemap from the query string
   const { hubURL } = req.query;
 
   // Make a GET request to your custom API to get the size of the feed
   const response = await fetch(`/api/size?feed=${hubURL}`);
-  const data = await response.json();
+  const data: SizeResponse = await response.json();
 
   // Print the size value to the page
   res.send(`The size of the feed is: ${data.size}`);
