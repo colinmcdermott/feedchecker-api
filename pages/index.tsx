7b602151b1f308e04fcd1caca262d371ece288ef
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-01-07 02:20:01 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,104 +1,67 @@
-import React, { useState, useEffect } from 'react';
-import Head from 'next/head';
+import { NextApiRequest, NextApiResponse } from 'next';
+import fetch from 'isomorphic-unfetch';
 
-function FeedChecker() {
-  const [hubURL, setHubURL] = useState('');
-  const [feedSize, setFeedSize] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [change, setChange] = useState('');
-  const [debugLink, setDebugLink] = useState('');
-  const [lastPing, setLastPing] = useState('');
+// Create a map to store the feed sizes in memory
+const feedSizes = new Map<string, number>();
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setLoading(true);
-    const response = await fetch(`/api/size?feed=${hubURL}`);
-    const data = await response.json();
-    setFeedSize(data.size);
-    setLoading(false);
-    setDebugLink(`https://pubsubhubbub.appspot.com/topic-details?hub.url=${hubURL}`);
-  };
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { feed } = req.query;
 
-  useEffect(() => {
-    if (feedSize !== null) { // only set up the interval if feedSize is not null
-      const interval = setInterval(async () => {
-        const response = await fetch(`/api/size?feed=${hubURL}`);
-        const data = await response.json();
-        if (data.size !== feedSize) {
-          setFeedSize(data.size);
-          setChange(`New feed size - Ping sent! - ${new Date().toUTCString()}`);
-          try {
-            // send ping to your new service
-            const pingResponse = await fetch(`/api/websub-ping?feed=${hubURL}`);
-            // handle pingResponse as needed
-            if (pingResponse.status !== 200) {
-              console.error(`Error pinging WebSub: ${pingResponse.status}`);
-              // handle error as needed
-            }
-          } catch (error) {
-            console.error(error);
-            // handle error as needed
-          }
-          // send ping to Google Sitemap API
-          const googlePingURL = `/api/google-ping?sitemap=${encodeURIComponent(hubURL)}`;
-          try {
-            const googlePingResponse = await fetch(googlePingURL);
-            // handle googlePingResponse as needed
-            if (googlePingResponse.status !== 200) {
-              console.error(`Error pinging Google: ${googlePingResponse.status}`);
-              // handle error as needed
-            }
-          } catch (error) {
-            console.error(error);
-            // handle error as needed
-          }
-          setLastPing(new Date().toUTCString());
-        } else {
-          setChange(`Feed size unchanged - ${new Date().toUTCString()}`);
-        }
-      }, 30000);
-      return () => clearInterval(interval);
+    // Make a request to the custom API to get the size of the feed
+    const response = await fetch(`https://nodefeedv.vercel.app/api/size?feed=${feed}`);
+
+    // Check the response status
+    if (response.status !== 200) {
+      // If the status is not 200 OK, return an error
+      res.status(response.status).json({ error: 'Failed to fetch feed' });
+      return;
     }
-  }, [feedSize, hubURL]);
 
-  return (
-    <main>
-      <Head>
-        <title>Node Feed Checker</title>
-      </Head>
-      <form className='feedCheckForm' onSubmit={handleSubmit}>
-        <label htmlFor='hubURL'>RSS Feed URL:</label>
-        <input
-          type='url'
-          name='hub.url'
-          placeholder='https://example.com/feed/'
-          pattern='https://.*'
-          id='hubURL'
-          value={hubURL}
-          onChange={(event) => setHubURL(event.target.value)}
-          required
-        />
-        <input type='submit' value='Submit' id='submit' />
-      </form>
+    const data = await response.json();
 
-      {loading && <div id='loading'><p>Connecting to API...</p></div>}
-      {feedSize && <div id='results'><p>API Success! Feed size: {feedSize} - Check interval: 30 seconds</p></div>}
+    // Check if the feed size is already stored in memory
+    let success = false; // initialize success to false
+    let storedSize: number | undefined; // store the value of storedSize in a separate variable
+    if (feedSizes.has(feed as string)) {
+      // If the feed size is already stored, compare it with the current size
+      storedSize = feedSizes.get(feed as string);
+      if (storedSize !== data.size) {
+        // If the sizes are different, update the stored size and send a ping to the WebSub API
+        feedSizes.set(feed as string, data.size);
+        console.log(`Sending pings for new feed size: ${data.size}`);
+        try {
+          // send ping to the WebSub API
+          const pingResponse = await fetch(`https://nodefeedv.vercel.app/api/websub-ping?feed=${feed}`);
+          // check if the ping was successful
+          success = pingResponse.status === 200;
+          // handle pingResponse as needed
+          if (!success) {
+            console.error(`Error pinging WebSub: ${pingResponse.status}`);
+          }
+        } catch (error) {
+          console.error(`Error pinging WebSub: ${error}`);
+        }
+      } else {
+        console.log(`Feed size is the same: ${data.size}`);
+      }
+    } else {
+      // If the feed size is not stored, add it to the map
+      feedSizes.set(feed as string, data.size);
+      console.log(`Stored new feed size: ${data.size}`);
+    }
 
-      <section className='statsWindow'>
-        <div id='change'>{change}</div>
-        {lastPing ? (
-          <div id='lastPing'>
-            Last ping sent: <time>{lastPing}</time>
-          </div>
-        ) : (
-          <div id='lastPing'></div>
-        )}
-      </section>
+    // Print the size in the console
+    console.log(data.size);
 
-      {debugLink && <div id='debug'><p><a href={debugLink} target='_blank'>Debug</a></p></div>}
-
-    </main>
-  );
-}
-
-export default FeedChecker;
+    // Respond with a success status and the size and success information in the body
+    res.status(200).send({ 
+      size: data.size, 
+      success: success, 
+      feedChanged: storedSize !== data.size // check if the feed size has changed
+    });
+  } catch (error) {
+    // If there was an error, return a server error
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
