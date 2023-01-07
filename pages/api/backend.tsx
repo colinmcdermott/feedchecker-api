16f97b8f519cc68c00f3d9ed292fd57015bafeb3
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-01-07 01:55:38 +0000

    Update backend.tsx

--- pages/api/backend.tsx
+++ pages/api/backend.tsx
@@ -21,7 +21,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const data = await response.json();
 
     // Check if the feed size is already stored in memory
-    let success = true; // assume the pings were successful unless an error occurs
+    let success;
     let storedSize: number | undefined; // store the value of storedSize in a separate variable
     if (feedSizes.has(feed as string)) {
       // If the feed size is already stored, compare it with the current size
