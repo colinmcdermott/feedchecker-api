2b80b9a0a20300b634185de61d97d6efd83177db
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-01-07 18:34:25 +0000

    Update backend.tsx

--- pages/api/backend.tsx
+++ pages/api/backend.tsx
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import fetch from 'isomorphic-unfetch';
+import querystring from 'querystring';
 
 // Create a map to store the feed sizes in memory
 const feedSizes = new Map<string, number>();
@@ -19,8 +20,11 @@ async function sendPing(feed: string) {
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    // Get the feed query parameter from the request
-    const feed = req.query.feed;
+    // Parse the query string of the request
+    const query = querystring.parse(req.url.split('?')[1]);
+
+    // Get the feed parameter from the query object
+    const feed = query.feed;
 
     // Make a request to the custom API to get the size of the feed
     const response = await fetch(`https://nodefeedv.vercel.app/api/size?feed=${feed}`);
@@ -37,23 +41,21 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     // Check if the feed size is already stored in memory
     let success = false; // initialize success to false
     let storedSize: number | undefined; // store the value of storedSize in a separate variable
-    if (typeof feed !== 'undefined') {
-      if (feedSizes.has(feed)) {
-        // If the feed size is already stored, compare it with the current size
-        storedSize = feedSizes.get(feed);
-        if (storedSize !== data.size) {
-          // If the sizes are different, update the stored size and send a ping to the WebSub API
-          feedSizes.set(feed, data.size);
-          console.log(`Sending pings for new feed size: ${data.size}`);
-          success = await sendPing(feed);
-        } else {
-          console.log(`Feed size is the same: ${data.size}`);
-        }
-      } else {
-        // If the feed size is not stored, add it to the map
+    if (feedSizes.has(feed)) {
+      // If the feed size is already stored, compare it with the current size
+      storedSize = feedSizes.get(feed);
+      if (storedSize !== data.size) {
+        // If the sizes are different, update the stored size and send a ping to the WebSub API
         feedSizes.set(feed, data.size);
-        console.log(`Stored new feed size: ${data.size}`);
+        console.log(`Sending pings for new feed size: ${data.size}`);
+        success = await sendPing(feed);
+      } else {
+        console.log(`Feed size is the same: ${data.size}`);
       }
+    } else {
+      // If the feed size is not stored, add it to the map
+      feedSizes.set(feed, data.size);
+      console.log(`Stored new feed size: ${data.size}`);
     }
 
     // Print the size in the console
@@ -67,7 +69,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     });
   } catch (error) {
     // If there was an error, return a server error
-        res.status(500).json({ error: 'Internal server error' });
-      }
-    };
-    
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
