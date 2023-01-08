132f149a68b2a31f41f6f89feca2cd6ebe6f3da2
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-01-08 19:50:07 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
+// next.config.js
+
+const apiMiddleware = (req, res, next) => {
+  res.setHeader('X-Robots-Tag', 'noindex');
+  next();
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverMiddleware: [
+    apiMiddleware
+  ],
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
