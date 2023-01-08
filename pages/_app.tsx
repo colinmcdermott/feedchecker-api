141cf31dbf09b68c1fbc39fa5fd2a30b816dac01
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-01-08 20:46:30 +0000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,8 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import localFont from '@next/font/local'
 
-const ibmPlexSans500 = localFont({ src: './fonts/ibm-sans-500.woff2' })
-const ibmPlexSans700 = localFont({ src: './fonts/ibm-sans-700.woff2' })
+const ibmPlexSans500 = localFont({ src: '../public/fonts/ibm-sans-500.woff2' })
+const ibmPlexSans700 = localFont({ src: '../public/fonts/ibm-sans-700.woff2' })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
