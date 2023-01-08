f466caa46606a6ae22fbfc1cfe5b56092f4c26c8
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-01-08 18:15:41 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -98,8 +98,9 @@ function FeedChecker() {
         ) : (
           <div id='lastPing'></div>
         )}
-        {debugLink && <div id='debug'><p><a href={debugLink} target='_blank'>Debug</a></p></div>}
       </section>
+
+      {debugLink && <div id='debug'><p><a href={debugLink} target='_blank'>Debug</a></p></div>}
       
       <section>
 
