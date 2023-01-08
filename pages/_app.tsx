ec5bd17a852cf2a9ba5438c15f30e0c6b391a075
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-01-08 21:39:25 +0000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import Head from 'next/head';
-import localFont from '@next/font/local';
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import localFont from '@next/font/local'
 
 const ibmPlexSans = localFont({
   src: [
@@ -18,23 +17,8 @@ const ibmPlexSans = localFont({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head>
-        {ibmPlexSans.fonts.map((font) => (
-          <link
-            key={font.path}
-            rel="preload"
-            href={font.path}
-            as="font"
-            type="font/woff2"
-            crossorigin
-            fetchPriority="high"
-          />
-        ))}
-      </Head>
-      <main className={ibmPlexSans.className}>
-        <Component {...pageProps} />
-      </main>
-    </>
+    <main className={ibmPlexSans.className}>
+      <Component {...pageProps} />
+    </main>
   );
 }
