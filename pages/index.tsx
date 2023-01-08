15b4d0b529f852ca0d3b124cdecc70648204b2b3
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-01-08 15:01:12 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -109,7 +109,7 @@ function FeedChecker() {
         <h3>API</h3>
         <p>The API takes a URL input, checks the size of the feed, then pings WebSub & Google if the feed has changed. The API stores the size of the file in memory so you can call the API server-side via a Cron job, without the risk of sending multiple pings.</p>
         <p>Send a request to the <code>/api/feedcache</code> API with a <code>feed</code> URL parameter and get a JSON response back. Eg <code>/api/feedcache?feed=https://example.com/feed/</code>.</p>
-        <p>A typical successful API response where the feed size is new and pings have been sent will look like this:</p> 
+        <p>A typical <em>successful API response</em> where the feed size is new and pings have been sent will look like this:</p> 
 
           <code>
               <pre>
