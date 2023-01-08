2846b20d4b286cdc1cab9b3faf99d237529a1132
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-01-08 00:43:52 +0000

    Create feed.tsx

---
+++ pages/api/feed.tsx
@@ -0,0 +1,42 @@
+const express = require('express');
+const request = require('request');
+
+const app = express();
+const feedSizes = new Map();
+
+app.get('/api/feed', (req, res) => {
+  const feed = req.query.feed;
+  if (!feed) {
+    return res.status(400).json({ error: 'Missing "feed" query parameter' });
+  }
+
+  // Check the size of the feed
+  request(`https://nodefeedv.vercel.app/api/size?feed=${feed}`, (error, response, body) => {
+    if (error) {
+      return res.status(500).json({ error: 'Error checking feed size' });
+    }
+
+    const size = JSON.parse(body).size;
+    const storedSize = feedSizes.get(feed);
+    const sizeChanged = storedSize !== size;
+
+    // Update the stored size in the map
+    feedSizes.set(feed, size);
+
+    // Fetch the WebSub API if the size has changed
+    if (sizeChanged) {
+      request(`https://nodefeedv.vercel.app/api/websub-fetch?feed=${feed}`, (error, response, body) => {
+        if (error) {
+          return res.status(500).json({ error: 'Error fetching WebSub API', size, sizeChanged });
+        }
+
+        return res.json({ size, sizeChanged, webSubFetchSuccess: true });
+      });
+    } else {
+      return res.json({ size, sizeChanged });
+    }
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`API listening on port ${port}!`));
