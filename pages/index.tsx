7336f252e0b761410f7191a43dc086c680660b75
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-01-08 04:09:39 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -64,7 +64,7 @@ function FeedChecker() {
   return (
     <main>
       <Head>
-        <title>Node Feed Checker</title>
+        <title>Feed Ping Tool</title>
         <link rel='canonical' href='https://feedping.dev/' />
         <link rel='icon' type='image/svg+xml' href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 width=%22256%22 height=%22256%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 dominant-baseline=%22central%22 text-anchor=%22middle%22 font-size=%2294%22>👋</text></svg>' />
       </Head>
