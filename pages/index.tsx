1ee99061b192a86a6e9b5241585a0389eace3934
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-01-08 17:13:59 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -69,7 +69,7 @@ function FeedChecker() {
         <link rel='icon' type='image/svg+xml' href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 width=%22256%22 height=%22256%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 dominant-baseline=%22central%22 text-anchor=%22middle%22 font-size=%2294%22>👋</text></svg>' />
       </Head>
       <section>
-        <h1>👋 Feed Ping Tool</h1>
+        <h1>Feed Ping Tool</h1>
         <form className='feedCheckForm' onSubmit={handleSubmit}>
           <label htmlFor='hubURL'>RSS Feed URL:</label>
           <input
