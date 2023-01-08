40a28acecdb2634521292658170b5c55292b9a0a
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-01-08 20:57:37 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// next.config.js
+module.exports = {
   reactStrictMode: true,
+  headers: [
+    {
+      source: '/fonts/ibm-sans-500.woff2',
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'public, max-age=31557600',
+        },
+      ],
+    },
+    {
+      source: '/fonts/ibm-sans-700.woff2',
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'public, max-age=31557600',
+        },
+      ],
+    },
+  ],
 }
-
-module.exports = nextConfig
