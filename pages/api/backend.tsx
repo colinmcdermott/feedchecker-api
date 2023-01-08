bfabcb54d59fde44ab215ac709a67b46de684c2b
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-01-08 00:07:53 +0000

    Update backend.tsx

--- pages/api/backend.tsx
+++ pages/api/backend.tsx
@@ -57,32 +57,24 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       // If the feed size is already stored, compare it with the current size
       storedSize = feedSizes.get(feed);
       if (storedSize !== data.size) {
-        // If the sizes are different, update the stored size and fetch the WebSub API
+      // If the feed size is not stored, add it to the map
+      if (typeof feed === 'string') {
         feedSizes.set(feed, data.size);
-        console.log(`Fetching WebSub API for new feed size: ${data.size}`);
-        success = await fetchWebSub(feed);
-      } else {
-        console.log(`Feed size is the same: ${data.size}`);
+        console.log(`Stored new feed size: ${data.size}`);
       }
-    } else {
-            // If the feed size is not stored, add it to the map
-            if (typeof feed === 'string') {
-              feedSizes.set(feed, data.size);
-              console.log(`Stored new feed size: ${data.size}`);
-            }
-            
-            // Print the size in the console
-            console.log(data.size);
-            
-            // Respond with a success status and the size and success information in the body
-          res.status(200).send({ 
-            size: data.size, 
-            success: success, 
-            feedChanged: storedSize !== data.size // check if the feed size has changed
-          });
-        } catch (error) {
-          // If there was an error, return a server error
-          res.status(500).json({ error: 'Internal server error' });
-        }
-      };
       
+      // Print the size in the console
+      console.log(data.size);
+      
+      // Respond with a success status and the size and success information in the body
+    res.status(200).send({ 
+      size: data.size, 
+      success: success, 
+      feedChanged: storedSize !== data.size // check if the feed size has changed
+    });
+  } catch (error) {
+    // If there was an error, return a server error
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
