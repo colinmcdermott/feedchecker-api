d9b0e9c115abdd6c498825130e7ec4b696d3d17d
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-01-10 01:06:57 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
-import { Content } from './Content';
+import { Content } from './content.tsx';
 
 function FeedChecker() {
   const [hubURL, setHubURL] = useState('');
