9b1ecda679df74618541012647f2efc13b054ca4
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-01-11 18:43:19 +0000

    Move head to sep file

---
+++ pages/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <Head>
+        <title>Feed Ping SEO Tool</title>
+        <meta name='description' content='Speed up your search engine indexing and content discovery with automated RSS and Sitemap pinging from FeedPing.dev.' />
+        <link rel='canonical' href='https://feedping.dev/' />
+        <link rel='icon' type='image/svg+xml' href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 width=%22256%22 height=%22256%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 dominant-baseline=%22central%22 text-anchor=%22middle%22 font-size=%2294%22>👋</text></svg>' />
+      </Head>
+    )
+}
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import HomeContent from './homecontent.js';
+import header from './header.js';
 
 function FeedChecker() {
   const [hubURL, setHubURL] = useState('');
@@ -64,12 +65,7 @@ function FeedChecker() {
 
   return (
     <main>
-      <Head>
-        <title>Feed Ping SEO Tool</title>
-        <meta name='description' content='Speed up your search engine indexing and content discovery with automated RSS and Sitemap pinging from FeedPing.dev.' />
-        <link rel='canonical' href='https://feedping.dev/' />
-        <link rel='icon' type='image/svg+xml' href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 width=%22256%22 height=%22256%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 dominant-baseline=%22central%22 text-anchor=%22middle%22 font-size=%2294%22>👋</text></svg>' />
-      </Head>
+      <Header />
 
       <form className='feedCheckForm' onSubmit={handleSubmit}>
         <label htmlFor='hubURL'>RSS Feed URL:</label>
