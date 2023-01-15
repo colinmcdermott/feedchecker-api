97bc4f4f569ab586cb427fd62a996d9ed4382f1d
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-01-15 04:00:09 +0000

    Moved API check to include

--- pages/api/rss.ts
+++ pages/api/rss.ts
@@ -4,6 +4,7 @@ import { Ratelimit } from '@upstash/ratelimit';
 import { Redis } from '@upstash/redis';
 const NodeCache = require('node-cache');
 import { MissingFeedParameterError, InvalidFeedParameterError, InvalidApiKeyError, FetchError } from './customerror'
+import checkApiKey from './checkApiKey';
 
 const app = express();
 const feedSizeCache = new NodeCache({ stdTTL: 14400 /* seconds */ });
@@ -15,42 +16,8 @@ const ratelimit = new Ratelimit({
   limiter: Ratelimit.slidingWindow(30, '1 h')
 });
 
-const apiKeys = ['U6M05O7nQabjMlGdJuo9UiSxFrgYdTak', 'tZ07kgxshMNtd2GLqqlr6FuquArxLGy1'];
-
-const checkApiKey = (req: Request, res: Response, next: NextFunction) => {
-  // Check if the API key is supplied in the HTTP headers
-  const apiKey = req.headers['feedping-api-key'] as string;
-  if (apiKeys.includes(apiKey)) {
-    // Set a flag in the request object indicating that the request is from a paid user
-    (req as any).isPaidUser = true;
-    // Add a header to the response indicating that a valid API key was provided
-    res.setHeader('feedping-api-key-valid', 'true');
-  } else {
-    // Check if the API key is supplied in the URL parameters
-    const apiKey = req.query.apiKey as string;
-    if (apiKeys.includes(apiKey)) {
-      // Set a flag in the request object indicating that the request is from a paid user
-      (req as any).isPaidUser = true;
-      // Add a header to the response indicating that a valid API key was provided
-      res.setHeader('feedping-api-key-valid', 'true');
-    } else {
-      // Add a header to the response indicating that a valid API key was not provided
-      res.setHeader('feedping-api-key-valid', 'false');
-      throw new InvalidApiKeyError();
-    }
-  }
-  const feed = req.query.feed as string;
-  if (!feed) {
-    throw new MissingFeedParameterError();
-  }
-  if (!feedRegex.test(feed)) {
-    throw new InvalidFeedParameterError();
-  }
-  next();
-};
-
 app.use(checkApiKey);
-  
+
 // Middleware function to handle rate limiting and request processing
 const handleRequest = async (req: Request, res: Response, next: NextFunction) => {
   const feed = req.query.feed as string;
@@ -74,17 +41,17 @@ const handleRequest = async (req: Request, res: Response, next: NextFunction) =>
     next(new FetchError());
   }
 };
-  
+
 // Centralized error handling middleware
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof MissingFeedParameterError) {
-    return res.status(400).json({ message: err.message });
+    return res.status(err.statusCode).json({ message: err.message });
   } else if (err instanceof InvalidFeedParameterError) {
-    return res.status(400).json({ message: err.message });
+    return res.status(err.statusCode).json({ message: err.message });
   } else if (err instanceof InvalidApiKeyError) {
-    return res.status(401).json({ message: err.message });
+    return res.status(err.statusCode).json({ message: err.message });
   } else if (err instanceof FetchError) {
-    return res.status(500).json({ message: err.message });
+    return res.status(err.statusCode).json({ message: err.message });
   } else {
     return res.status(500).json({ message: 'An unexpected error occurred' });
   }
